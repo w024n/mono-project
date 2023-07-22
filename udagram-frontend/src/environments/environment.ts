12e55d6c32ab8d823c7c68293efa331ce3a2ab6b
w024n <w024sec@gmail.com>
2023-07-22 12:07:52 +0800

    Changes to be committed:

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a55f08fcf59754381b885121705cb60c-2147251900.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
