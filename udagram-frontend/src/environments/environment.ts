5abff6f1dba369ac1640bb3a6ab15975605dc1f2
w024n <133731040+w024n@users.noreply.github.com>
2023-07-22 12:23:04 +0800

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a55f08fcf59754381b885121705cb60c-2147251900.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://afc006fc522b74eadbfa90acc16073e6-487429328.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
